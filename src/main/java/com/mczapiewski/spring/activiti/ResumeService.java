39a35d69c93960b831619f633ac3844402799ef3
Marcin Czapiewski <m.czapiewski@striped-giraffe.com>
2015-12-03 13:12:59 +0100

    Przykladowy projekt

---
+++ src/main/java/com/mczapiewski/spring/activiti/ResumeService.java
@@ -0,0 +1,14 @@
+package com.mczapiewski.spring.activiti;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResumeService {
+	private static final Logger LOGGER = Logger.getLogger(ResumeService.class);
+	
+    public void storeResume() {
+//        System.out.println("Storing resume ...");
+        LOGGER.info("Storing resume ...");
+    }
+}
